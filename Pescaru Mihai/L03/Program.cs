a4f22986fafae04570003ed17b347b2b1b18875c
pescaru98 <mihai.pescaru@elysian-software.com>
2020-10-13 01:04:09 +0300

    L03 Modificari: adaugat citire toate fisierele

--- Pescaru Mihai/L03/Program.cs
+++ Pescaru Mihai/L03/Program.cs
@@ -1,16 +1,27 @@
-﻿using System;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
 
 namespace L03
 {
     class Program
     {
+        private static DriveService _service;
+        private static string _token;
+
         static void Main(string[] args)
         {
             init();
-            Console.WriteLine("Hello World!");
+            readAllData();
         }
 
-        static void init(){
+        static void init()
+        {
             string[] scopes = new string[]{
                 DriveService.Scope.Drive,
                 DriveService.Scope.DriveFile
@@ -19,25 +30,66 @@ namespace L03
             var clientId = "435716270694-9ns3l5d0oterjt00js0hdnpas8g6hhce.apps.googleusercontent.com";
             var clientSecret = "1Dc_22hM_Te62-noGDKfk2b6";
 
-              var credential = GoogleWebAuthorizationBroker.AuthorizeASync(
-                  new ClientSecrets
-                  {
-                    ClientId = clientId,
-                    ClientSecret = clientSecret 
-                  },
-                  scopes,
-                  Environment.UserName,
-                  CancellationToken.None,
-                  null
-              ).Result;
+            var credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                new ClientSecrets
+                {
+                    ClientId = clientId, 
+                    ClientSecret = clientSecret
+                },
+                scopes,
+                Environment.UserName,
+                CancellationToken.None,
+                null
+            ).Result;
 
-              _service = new DriveService(new Google.Apis.Services.BaseClientService.Initializer(){
+            _service = new DriveService(new Google.Apis.Services.BaseClientService.Initializer()
+            {
                 HttpClientInitializer = credential
-              });
+            });
 
-              _token = credential.Token.AccessToken;
-			  
-			  Console.Write("Token: "+credential.Token.AccessToken);
+            _token = credential.Token.AccessToken;
+             
+            
         }
+
+        static void readAllData()
+        {
+            Console.WriteLine("Token: " + _token);
+
+            var request = (HttpWebRequest)WebRequest.Create("https://www.googleapis.com/drive/v3/files");
+                 //request.PreAuthenticate = true;
+                request.Headers.Add("Authorization", "Bearer " + _token);
+                request.Accept = "application/json";
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+
+            Console.WriteLine(responseString);
+        }
+
+        static async void readDataOfFile(string fileId)
+        {
+            HttpClient client = new HttpClient();
+            var responseString = await client.GetStringAsync("https://www.googleapis.com/drive/v3/files/"+fileId);
+
+            Console.WriteLine(responseString);
+        }
+
+/*        static async void writeInFile(string fileName, string message)
+        {
+
+                         HttpClient client = new HttpClient();
+              var values = new Dictionary<string, string>
+            {
+                { "fileId",fileId }
+            };
+
+            var content = new FormUrlEncodedContent(values);
+            var response = await client.PostAsync("http://www.example.com/recepticle.aspx", content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+        }*/
     }
 }
